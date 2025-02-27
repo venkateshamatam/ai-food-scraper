11b75509c49adbea576d618bc4b17a9578c234f5
venkateshamatam <matam.v@northeastern.edu>
2025-02-26 23:39:19 -0500

    Add UI assets

---
+++ frontend/assets/badges/badge-ke.tsx
@@ -0,0 +1,22 @@
+export default function KEBadge() {
+	return (
+	  <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<circle cx="15" cy="15" r="15" fill="#DD7862" fillOpacity="0.7" />
+		<text
+		  x="15"
+		  y="15"
+		  dominantBaseline="central"
+		  textAnchor="middle"
+		  fill="black"
+		  style={{
+			fontFamily: "'Panel Sans', sans-serif",
+			fontWeight: 700,
+			fontSize: "10px",
+			letterSpacing: "0px",
+		  }}
+		>
+		  KE
+		</text>
+	  </svg>
+	)
+  }  
